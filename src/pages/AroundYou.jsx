73d4df5812ba33588ca928b7569544b6b3f076f4
Justin Johnson <threejay20@yahoo.ca>
2023-04-24 10:26:43 -0400

    Use State and Country

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -4,7 +4,18 @@ import { useSelector } from 'react-redux';
 import {Error, Loader, SongCard} from '../components';
 import React from 'react';
 
-const AroundYou = () =>
+const AroundYou = () =>{
+
+const [country, setCountry] = useState('');
+const [loading, setLoading] = useState(true);
+const {activeSong, isPlaying} = useSelector ((state) => state.player);
+
+return (
+    <div>
+
+    </div>
+)
+}
 
 const CountryTracks = () => <div>CountryTracks</div>;
 
