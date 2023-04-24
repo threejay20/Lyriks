ac8cce7cde9e7e1caeea07994f4131c0b13a1293
Justin Johnson <threejay20@yahoo.ca>
2023-04-24 13:50:53 -0400

    Artist Card update

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -6,9 +6,11 @@ const ArtistCard = ({ track }) => {
 
   return (
     <div className="flex flex-col w-[250] p-4 bg-white/5 
-    bg-opacity-80 backdrop-blue-sm animate-slideup rounded-lg cursor-pointer">
+    bg-opacity-80 backdrop-blue-sm animate-slideup rounded-lg cursor-pointer" 
+    onClick={() => navigate(`/artists/${track?.artists[0].adamid}`)}>
 
       <img alt="artist" src={track?.images?.coverart} className="w-full h-56 rounded-lg" />
+      <p className="mt-4 font-semibold text-lg text-white truncate"> {track?.subtitle}</p>
     </div>
   )
 };
