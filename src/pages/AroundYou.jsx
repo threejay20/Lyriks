962d4e9b3d0295e5efcf383c1ac08b02b05e14d7
Justin Johnson <threejay20@yahoo.ca>
2023-04-24 09:34:55 -0400

    Around You Start

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,4 +1,10 @@
+import {useState, useEffect} from 'react'
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+import {Error, Loader, SongCard} from '../components';
 import React from 'react';
+
+const AroundYou = () =>
 
 const CountryTracks = () => <div>CountryTracks</div>;
 
