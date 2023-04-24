002e3747858249178cc053a2dcf07914ec2a875b
Justin Johnson <threejay20@yahoo.ca>
2023-04-24 14:19:05 -0400

    genre drop down

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -2,6 +2,8 @@ import { Error, Loader, SongCard } from "../components";
 import { genres } from '../assets/constants';
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 import { useDispatch, useSelector } from "react-redux";
+import { selectGenreListId } from "../redux/features/playerSlice";
+
 
 
 
@@ -10,7 +12,7 @@ import { useDispatch, useSelector } from "react-redux";
 
 const Discover = () => { 
     const dispatch = useDispatch ();
-    const { activeSong, isPlaying } = useSelector ((state) => state.player);
+    const { activeSong, isPlaying, genreListId } = useSelector ((state) => state.player);
 const { data, isFetching, error } = useGetTopChartsQuery ();
 const genreTitle = 'Pop';
 
@@ -25,8 +27,8 @@ return(
 
 <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
     <h2 className="font-bold text-3x1 text-white text-left">Discover {genreTitle}</h2>
-    <select onChange={() => {}}
-    value= ""
+    <select onChange={(e) => dispatch (selectGenreListId(e.target.value))}
+    value= {genreListId || 'pop'}
     className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
     >
     {genres.map((genre) => <option key={genre.value} value={genre.value}> {genre.title}
