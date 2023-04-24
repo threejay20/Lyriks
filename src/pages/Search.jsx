1fb07106e31ae0ca114142de3dd2fb8b0308888c
Justin Johnson <threejay20@yahoo.ca>
2023-04-24 15:31:44 -0400

    fixed no results

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -4,27 +4,31 @@ import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import {Error, Loader, SongCard} from '../components';
 import React from 'react';
-import { useGetTopChartsQuery } from '../redux/services/shazamCore';
+import { useGetSongsBySearchQuery } from '../redux/services/shazamCore';
 
-const Search = () =>{
-
+const Search = () => {
+const {searchTerm} = useParams();
 
 const {activeSong, isPlaying} = useSelector ((state) => state.player);
-const {data, isFetching, error } = useGetTopChartsQuery ();
+const {data, isFetching, error } = useGetSongsBySearchQuery (searchTerm);
+
+const songs = data?.tracks?.hits?.map((song) => song.track);
 
 
+if(isFetching) return <Loader title= {`Searching ${searchTerm}`} />;
 
-if(isFetching) return <Loader title="Loading top charts"/>;
-
-if (error && country) return <Error/>
+if (error)  return <Error/>
 
 return (
     <div className="flex flex-col">
-        <h2 className="font-bold text-3x1 text-white text-left mt-4 mb-10">Discover Top Charts
+        <h2 className="font-bold text-3x1 text-white text-left mt-4 mb-10">
+          Showing results for 
+          
+          <span className="font-black"> {searchTerm} </span>
         </h2>
 
         <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-            {data?.map((song, i) => (
+            {songs?.map((song, i) => (
                 <SongCard
                 key={song.key}
                 song={song}
@@ -35,5 +39,9 @@ return (
                 />
             ))}
         </div>
+        </div>
+        );
+        };
+        
 
 export default Search;
