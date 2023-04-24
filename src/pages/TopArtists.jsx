288196dd8534e48635eb760ea8757a6ada9179d1
Justin Johnson <threejay20@yahoo.ca>
2023-04-24 13:39:32 -0400

    Top Artists

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -1,3 +1,40 @@
-const TopArtists = () => <div>TopArtists</div>;
+
+import axios from 'axios';
+import {Error, Loader, ArtistCard} from '../components';
+import React from 'react';
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
+
+const TopArtists = () =>{
+
+
+const {data, isFetching, error } = useGetTopChartsQuery ();
+
+
+
+if(isFetching) return <Loader title="Loading top charts"/>;
+
+if (error && country) return <Error/>
+
+return (
+    <div className="flex flex-col">
+        <h2 className="font-bold text-3x1 text-white text-left mt-4 mb-10">Top Artists
+        </h2>
+
+        <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+            {data?.map((track) => (
+                <ArtistCard
+                key={track.key}
+                track={track}
+                
+                />
+            ))}
+        </div>
+
+
+    </div>
+)
+}
+
+
 
 export default TopArtists;
--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -1,3 +1,45 @@
-const TopCharts = () => <div>TopCharts</div>;
+
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+import {Error, Loader, SongCard} from '../components';
+import React from 'react';
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
+
+const TopCharts = () =>{
+
+
+const {activeSong, isPlaying} = useSelector ((state) => state.player);
+const {data, isFetching, error } = useGetTopChartsQuery ();
+
+
+
+if(isFetching) return <Loader title="Loading top charts"/>;
+
+if (error && country) return <Error/>
+
+return (
+    <div className="flex flex-col">
+        <h2 className="font-bold text-3x1 text-white text-left mt-4 mb-10">Discover Top Charts
+        </h2>
+
+        <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+            {data?.map((song, i) => (
+                <SongCard
+                key={song.key}
+                song={song}
+                isPlaying={isPlaying}
+                activeSong={activeSong}
+                data={data}
+                i={i}
+                />
+            ))}
+        </div>
+
+
+    </div>
+)
+}
+
+
 
 export default TopCharts;
